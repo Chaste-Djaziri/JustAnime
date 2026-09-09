04f31b1ef6d4f6df57eaed2c92f4c3b8af5a1fb7
Chaste-Djaziri <habimanahirwa@gmail.com>
2026-09-09 11:56:50 +0200

    Add consumet proxy to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./"),
     },
   },
+  server: {
+    proxy: {
+      "/consumet": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/consumet/, ""),
+      },
+    },
+  },
 })
