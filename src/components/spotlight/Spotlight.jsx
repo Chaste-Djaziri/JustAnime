0effeaaaac2af0d160be1e745de89712a3554977
Chaste-Djaziri <habimanahirwa@gmail.com>
2026-09-09 13:53:20 +0200

    feat(spotlight): add rounded corners container and remove pagination dots

--- src/components/spotlight/Spotlight.jsx
+++ src/components/spotlight/Spotlight.jsx
@@ -1,63 +1,50 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Autoplay, Pagination, EffectFade } from "swiper/modules";
+import { Autoplay, EffectFade } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/autoplay";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 import "swiper/css/effect-fade";
 import "./Spotlight.css";
 import Banner from "../banner/Banner";
 
 const Spotlight = ({ spotlights }) => {
   return (
-    <>
-      <div className="relative h-[420px] sm:h-[460px] md:h-[500px] lg:h-[540px] w-full overflow-hidden bg-[#0a0a0a]">
+    <div className="w-full relative">
+      <div className="relative h-[400px] sm:h-[440px] md:h-[480px] lg:h-[520px] w-full rounded-2xl md:rounded-3xl overflow-hidden border border-white/10 shadow-2xl bg-[#0a0a0a]">
         {spotlights && spotlights.length > 0 ? (
-          <>
-            <Swiper
-              spaceBetween={0}
-              slidesPerView={1}
-              loop={true}
-              allowTouchMove={true}
-              breakpoints={{
-                1024: {
-                  allowTouchMove: false,
-                },
-              }}
-              navigation={{
-                nextEl: ".button-next",
-                prevEl: ".button-prev",
-              }}
-              pagination={{
-                clickable: true,
-                dynamicBullets: false,
-              }}
-              autoplay={{
-                delay: 10000,
-                disableOnInteraction: false,
-              }}
-              effect="fade"
-              fadeEffect={{ crossFade: true }}
-              modules={[Navigation, Autoplay, Pagination, EffectFade]}
-              className="h-full w-full overflow-hidden relative"
-              style={{
-                "--swiper-pagination-bullet-inactive-color": "rgba(255, 255, 255, 0.5)",
-                "--swiper-pagination-bullet-inactive-opacity": "1",
-              }}
-            >
-              {spotlights.map((item, index) => (
-                <SwiperSlide className="text-black relative" key={index}>
-                  <Banner item={item} index={index} total={spotlights.length} />
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </>
+          <Swiper
+            spaceBetween={0}
+            slidesPerView={1}
+            loop={true}
+            allowTouchMove={true}
+            breakpoints={{
+              1024: {
+                allowTouchMove: false,
+              },
+            }}
+            navigation={false}
+            pagination={false}
+            autoplay={{
+              delay: 8000,
+              disableOnInteraction: false,
+            }}
+            effect="fade"
+            fadeEffect={{ crossFade: true }}
+            modules={[Autoplay, EffectFade]}
+            className="h-full w-full overflow-hidden relative"
+          >
+            {spotlights.map((item, index) => (
+              <SwiperSlide className="text-black relative h-full w-full" key={index}>
+                <Banner item={item} index={index} total={spotlights.length} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         ) : (
-          <p>No spotlights to show.</p>
+          <p className="text-zinc-500 p-8">No spotlights to show.</p>
         )}
       </div>
-    </>
+    </div>
   );
 };
 
 export default Spotlight;
+
